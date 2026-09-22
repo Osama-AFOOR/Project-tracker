be6c8f1ca0a34f2b338abfeac86c54af236f04b3
Osama-AFOOR <osos1011@gmail.com>
2026-09-22 19:17:56 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -294,7 +294,7 @@ app.delete("/admin/comments/:taskId/:commentId", auth, authorizeRoles("Admin"), 
 // --------------------
 // Server Start
 // --------------------
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
