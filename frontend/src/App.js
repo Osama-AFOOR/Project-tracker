6a8431c3eac751587af29220ef29fd716295d611
Osama-AFOOR <osos1011@gmail.com>
2026-09-26 18:44:58 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -212,7 +212,7 @@ function App() {
 <div className="sort-container">
   <label>Sort by: </label>
   <select
-    defaultValue="date"   // ✅ default sorting is Critical First
+    defaultValue="critical"   // ✅ default sorting is Critical First
     onChange={e => {
       const value = e.target.value;
       let sorted = [...tasks];
